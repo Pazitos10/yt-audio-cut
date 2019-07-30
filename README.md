1139fe34a83f677983a844b11575b9d625feeb12
Bruno <pazosbruno@gmail.com>
2019-07-29 21:39:02 -0300

    updated README.md + script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ytacut
 Download and cut audio from a yt video
+
+### How to use:
+
+    $ ./ytacut.sh -h
+
+    Usage: ./ytacut.sh -u <URL> -f <from_timestamp> -t <to_timestamp> -o <output_filename>
+    Example: ./ytacut.sh -u <URL> -f 00:01:30 -t 00:02:00 -o test.mp3
+    Dependencies: youtube-dl and ffmpeg
---
+++ ytacut.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Download and cut audio from a yt video
+
+print_usage(){
+    echo "Usage: ./ytacut.sh -u <URL> -f <from_timestamp> -t <to_timestamp> -o <output_filename>"
+    echo "Example: ./ytacut.sh -u <URL> -f 00:01:30 -t 00:02:00 -o test.mp3"
+    echo "Dependencies: youtube-dl and ffmpeg"
+}
+
+while getopts ':u:f:t:o:h' flag; do
+  case "${flag}" in
+    u) url=$2;;
+    f) from=$4;;
+    t) to=$6;;
+    o) output_filename=$8;;
+    h) print_usage && exit;;
+  esac
+done
+
+if [ -z "$url" ]
+then
+    echo "You must provide the yt video URL in order to extract the audio."
+    echo "Run ./ytacut.sh -h for help"
+else
+    if [ -z "$output_filename" ]
+    then
+        output_filename="audio_cut.mp3"
+    fi
+    echo "[1/3] Downloading video from $url"
+    ytdl_output=$((youtube-dl -x --audio-format mp3 $url) 2>&1) 
+    filename=$(echo $ytdl_output | egrep -Eo "\[ffmpeg\] Destination: .*.mp3" | cut -d ":" -f 2 | cut -c 1 --complement)
+    echo "[2/3] Preparing to cut"
+    if [ -z "$from" ] || [ -z "$to" ]
+    then
+        echo "The flags -f and/or -t were not used, cutting process not executed."
+    else
+        ffmpeg -i $filename -ss $from -to $to -y -c copy $output_filename -loglevel quiet
+    fi
+    echo "[3/3] Done! :D"
+fi 
